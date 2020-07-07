ce96ab0f05c4ec4e3296c53cfba6ac51a5e73a69
Entra21 <entra21@proway.treina>
2020-07-06 22:13:24 -0300

    Aprendendo os tipos básicos

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace primeira_aula
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var name = "Tatiane";
+            var age = 28;
+            var grade = 9.5;
+            var study = true;
+
+            Console.WriteLine("Qual é o seu nome?");
+            var userName = Console.ReadLine();
+
+            Console.WriteLine($"Olá, seja bem vindo {userName}!");
+            Console.WriteLine("Você é maior de idade?");
+            var amIlegalAge = Console.ReadLine() == "sim";
+
+            Console.WriteLine(study);
+        }
+    }
+}
